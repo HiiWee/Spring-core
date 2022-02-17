59664ddb85dfe92ed198a8f8462c3cb0845af04f
HiiWee <wpdlzhf159@gmail.com>
2022-02-17 19:36:59 +0900

    Singleton container(Spring container)

--- core/src/test/java/hello/core/singleton/SingletonTest.java
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -4,11 +4,12 @@ import hello.core.AppConfig;
 import hello.core.member.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
 class SingletonTest {
-    
+
     @Test
     @DisplayName("스프링 없는 순수한 DI 컨테이너")
     void pureContainer() {
@@ -18,7 +19,7 @@ class SingletonTest {
 
         // 2. 조회: 호출 할 때 마다 객체를 생성
         MemberService memberService2 = appConfig.memberService();
-        
+
         // 참조값이 다른 것을 확인
         System.out.println("memberService1 = " + memberService1);
         System.out.println("memberService2 = " + memberService2);
@@ -29,5 +30,31 @@ class SingletonTest {
 
         // memberService1 != memberService2 검증
         assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        // pure
+        // AppConfig appConfig = new AppConfig();
+        // spring
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // 1. 조회: 호출 할 때마다 같은 객체 반환
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        // 2. 조회: 호출 할 때 마다 같은 객체 반환
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // 참조값이 같은 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // 실제로 참조값이 다르게 된다. 이렇게 되면 JVM메모리에 계속 객체가 생성되어 올라간다.
+        // 여기서 발생하는 문제? : 스프링은 웹 애플리케이션을 주로 만든다. 이의 특징은 고객의 요청이 매우 많다.
+        // 따라서 이런식으로 계속 객체를 생성하는 방식은 효율적이지 못하다.
+
+        // memberService1 == memberService2 검증
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
