46ef00466104f9de12ec3cb8f74543b1d60dbe96
HiiWee <wpdlzhf159@gmail.com>
2022-02-19 00:19:52 +0900

    싱글턴 방식의 주의점

--- core/src/test/java/hello/core/singleton/SingletonStateServiceTest.java
+++ core/src/test/java/hello/core/singleton/SingletonStateServiceTest.java
@@ -1,6 +1,7 @@
 package hello.core.singleton;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 class SingletonStateServiceTest {
 
     @Test
+    @DisplayName("싱글턴 방식의 주의점 : stateful한 설계")
     void statefulServiceSingleton() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
         StatefulService statefulService1 = ac.getBean(StatefulService.class);
@@ -26,15 +28,22 @@ class SingletonStateServiceTest {
     }
 
     @Test
+    @DisplayName("싱글턴 방식의 주의점 해결 : stateless(무상턔)한 설계")
     void statelessServiceSingleton() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
         StatelessService statelessService1 = ac.getBean(StatelessService.class);
         StatelessService statelessService2 = ac.getBean(StatelessService.class);
 
+        // ThreadA : A사용자 10000원 주문
         int userAPrice = statelessService1.order("userA", 10000);
+        // ThreadB : B사용자 20000원 주문
         int userBPrice = statelessService2.order("userA", 20000);
 
+        // ThreadA : A사용자 주문 금액 조회 -> 공유 필드를 사용하지 않으므로 10000원 정삭 출력됨
         System.out.println("userAPrice = " + userAPrice);
+
+        // UserA와 USerB의 주문 가격이 다름을 검증한다.
+        Assertions.assertThat(userAPrice).isNotSameAs(userBPrice);
 
     }
 
--- core/src/test/java/hello/core/singleton/StatelessService.java
+++ core/src/test/java/hello/core/singleton/StatelessService.java
@@ -3,14 +3,15 @@ package hello.core.singleton;
 // 싱글턴 방식의 주의점 해결 : stateless한 설계
 public class StatelessService {
 
-    // private int price; // 상태를 유지하는 필드.
+    // private int price; // 상태를 유지하는 필드를 제거함 : 싱글톤에선 공유 필드가 되므로
 
     public int order(String name, int price) {
         System.out.println("name = " + name + " price = " + price);
-        // this.price = price; // 여기가 문제 : stateful한 설계
-        return price;
+        // this.price = price; // 여기가 문제 : stateful한 설계를 지우고 값 자체를 반환
+        return price; // 바로 값을 반환하여 stateful한 문제를 해결
     }
 
+    // 사용하지 않는 메소드
 //    public int getPrice() {
 //        return price;
 //    }
