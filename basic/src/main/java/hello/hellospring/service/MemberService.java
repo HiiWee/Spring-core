cf844121b195dc32641fba9208e25ba60ca34653
HiiWee <wpdlzhf159@gmail.com>
2022-03-28 00:51:50 +0900

    AOP가 필요한 상황

--- basic/src/main/java/hello/hellospring/service/MemberService.java
+++ basic/src/main/java/hello/hellospring/service/MemberService.java
@@ -25,11 +25,18 @@ public class MemberService {
      * 회원 가입
      */
     public Long join(Member member) {
-        // 같은 이름이 있는 중복 회원 x
 
-        validateDuplicateMember(member);    // 중복회원 검증
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+
+        try {
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
     }
 
     /**
@@ -48,7 +55,15 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = " + timeMs + "ms");
+        }
     }
 
     /**
